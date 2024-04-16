9fbbb5f033e19f2c65cb0e08746ee541d08e1ffa
Harshal Atkar <142751341+HarshalAtkar@users.noreply.github.com>
2024-04-16 11:22:12 +0000

    solutin

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [text,setText]=useState("");
+ 
+  const coppytext=(event)=>{
+    setText(event.target.value);
+
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+        <input onKeyup={coppytext} ke />
+        <p>{text}</p>
     </div>
   )
 }
