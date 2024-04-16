f83ed196154b2aa97862ceb5366c073b94dadd55
Harshal Atkar <142751341+HarshalAtkar@users.noreply.github.com>
2024-04-16 11:46:53 +0000

    added

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,7 @@ const App = () => {
     <div>
       <label htmlFor="name">Enter your name:</label>
         <input id="name" onKeyUp={coppytext} />
-        {text ? <p>Hello {text}!</p> : null}
+        <p>Hello {text}!</p>
     </div>
   )
 }
